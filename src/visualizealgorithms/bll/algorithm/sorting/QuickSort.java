784ac8d7f521185947eebcc1263479808c41a573
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-03-14 15:50:24 +0100

    l

---
+++ src/visualizealgorithms/bll/algorithm/sorting/QuickSort.java
@@ -0,0 +1,16 @@
+package visualizealgorithms.bll.algorithm.sorting;
+
+import visualizealgorithms.bll.algorithm.AlgorithmType;
+import visualizealgorithms.bll.algorithm.GenericAlgorithm;
+
+public class QuickSort extends GenericAlgorithm {
+
+    public QuickSort() {
+        super("Quick Sort", "Fast sorting - O(n log n", AlgorithmType.SORTING);
+    }
+
+    @Override
+    public void doWork() {
+
+    }
+}
---
+++ src/visualizealgorithms/bll/algorithm/sorting/TowerOfHanoi.java
@@ -0,0 +1,15 @@
+package visualizealgorithms.bll.algorithm.sorting;
+
+import visualizealgorithms.bll.algorithm.AlgorithmType;
+import visualizealgorithms.bll.algorithm.GenericAlgorithm;
+
+public class TowerOfHanoi extends GenericAlgorithm {
+    public TowerOfHanoi() {
+        super("Tower of Hanoi", "slow - O(2^n)", AlgorithmType.SORTING);
+    }
+
+    @Override
+    public void doWork() {
+
+    }
+}
